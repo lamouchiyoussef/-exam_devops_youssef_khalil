359462191609c970d48e6c5e460f264a30c9bb26
lamouchi.youssef <58522491+lamouchiyoussef@users.noreply.github.com>
2022-01-05 12:56:09 +0100

    therd commit

--- src/test/java/com/devops/calculator/CalculatorServiceTest.java
+++ src/test/java/com/devops/calculator/CalculatorServiceTest.java
@@ -19,17 +19,17 @@ public class CalculatorServiceTest {
     }
      @Test
     public void testSub() {
-        assertEquals(15, new CalculatorService().Sub(10, 5).getResult());
+        assertEquals(5, new CalculatorService().Sub(10, 5).getResult());
     }
  @Test
     public void testMull() {
-        assertEquals(15, new CalculatorService().Mull(10, 5).getResult());
+        assertEquals(50, new CalculatorService().Mull(10, 5).getResult());
     }
 
  
  @Test
     public void testDiv() {
-        assertEquals(15, new CalculatorService().Div(10, 5).getResult());
+        assertEquals(2, new CalculatorService().Div(10, 5).getResult());
     }
 
 }
