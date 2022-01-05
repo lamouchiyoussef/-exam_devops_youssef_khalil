e02bb740a1d592a8e8a8194df9082d14c9e3163c
lamouchi.youssef <58522491+lamouchiyoussef@users.noreply.github.com>
2022-01-05 12:35:23 +0100

    first commit

---
+++ src/test/java/com/devops/calculator/CalculatorServiceTest.java
@@ -0,0 +1,38 @@
+package com.devops.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+public class CalculatorServiceTest {
+
+    @Test
+    public void testPing() {
+        assertThat(new CalculatorService().ping(), containsString("Welcome to Java Maven Calculator Web App!!!"));
+    }
+
+    @Test
+    public void testAdd() {
+        assertEquals(15, new CalculatorService().Add(10, 5).getResult());
+    }
+     @Test
+    public void testSub() {
+        assertEquals(15, new CalculatorService().Sub(10, 5).getResult());
+    }
+ @Test
+    public void testMull() {
+        assertEquals(15, new CalculatorService().Mull(10, 5).getResult());
+    }
+
+ @Test
+    public void testAdd() {
+        assertEquals(15, new CalculatorService().Add(10, 5).getResult());
+    }
+ @Test
+    public void testDiv() {
+        assertEquals(15, new CalculatorService().Div(10, 5).getResult());
+    }
+
+}
